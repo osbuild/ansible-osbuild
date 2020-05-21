374623935a72664da60689bfbf9d13a351a38fd1
Major Hayden <major@redhat.com>
2020-05-21 09:35:39 -0500

    Deploy to RHEL 8.3
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot/run_tests.sh
+++ schutzbot/run_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+hold_node() {
+  ip addr
+  sleep 3600
+  exit 1
+}
+trap "hold_node" ERR
+
 # Get OS details.
 source /etc/os-release
 
@@ -9,8 +16,8 @@ source /etc/os-release
 REPO_BASE_URL=https://rhos-d.infra.prod.upshift.rdu2.redhat.com:13808/v1/AUTH_95e858620fb34bcc9162d9f52367a560/osbuildci-artifacts
 sudo tee /etc/yum.repos.d/osbuild-mock.repo > /dev/null << EOF
 [osbuild-mock]
-name=osbuild mock ${BUILD_TAG} ${ID}${VERSION_ID//./}
-baseurl=${REPO_BASE_URL}/jenkins-osbuild-osbuild-composer-PR-651-26/${ID}${VERSION_ID//./}
+name=osbuild mock
+baseurl=${REPO_BASE_URL}/jenkins-osbuild-osbuild-composer-PR-657-1/${ID}${VERSION_ID//./}
 enabled=1
 gpgcheck=0
 # Default dnf repo priority is 99. Lower number means higher priority.
@@ -31,6 +38,10 @@ ansible-playbook -i hosts.ini ${EXTRA_VARS:-} playbook.yml
 # Mount a ramdisk on /run/osbuild to speed up testing.
 sudo mkdir -p /run/osbuild
 sudo mount -t tmpfs tmpfs /run/osbuild
+
+# Replace the RHEL 8.3 repository file.
+sudo cp schutzbot/rhel-8.3.json \
+    /usr/share/osbuild-composer/repositories/rhel-8.3.json
 
 # Once everything is deployed, add a blueprint and build it just to ensure
 # everything is working.
